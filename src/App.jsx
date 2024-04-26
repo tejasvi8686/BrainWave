8a6e1252d32ea852014994e22b28b5e4ff51eecf
tejavi8686 <tejasviraj8686@gmail.com>
2024-04-26 09:46:31 +0530

    projet setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl font-bold underline">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
