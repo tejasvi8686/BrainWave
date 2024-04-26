fd0126d546b04d27ca426b3425520831f8f25be9
tejavi8686 <tejasviraj8686@gmail.com>
2024-04-26 23:10:13 +0530

    benefits section & collaboration section done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Button from "./components/Button";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <div className="pt-[4.75rem] lg:[5.25] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
+        <Collaboration />
       </div>
       <ButtonGradient />
     </>
