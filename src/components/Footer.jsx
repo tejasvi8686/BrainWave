8811cc4ed69438ca68083906654ecf38ce8bfe4c
tejavi8686 <tejasviraj8686@gmail.com>
2024-04-27 08:23:02 +0530

    changes in fotter section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Section from "./section";
 import { socials } from "../constants";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -9,14 +10,17 @@ const Footer = () => {
         <p className="caption text-n-4 lg:block">
           © {new Date().getFullYear()}. All rights reserved.
         </p>
-        <p className="caption">❤️ Crafted by Tejasvi Raj</p>
+        <p className="caption text-n-3">
+          ❤️ Crafted by{" "}
+          <Link target="_blank" to="https://tejasviraj.vercel.app/">Tejasvi Raj</Link>{" "}
+        </p>
 
         <ul className="flex gap-5 flex-wrap">
           {socials.map((item) => (
             <a
               key={item.id}
               href={item.url}
-              target="_blank"
+              
               className="flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6"
             >
               <img src={item.iconUrl} width={16} height={16} alt={item.title} />
